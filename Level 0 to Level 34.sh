45a7eb1f6993b740b037c643f3cc5ae408b453de
Tanjim Hasan Masum <119100205+masumtanjim7@users.noreply.github.com>
2025-08-13 03:41:03 +0600

    Masum

---
+++ Level 0 to Level 34.sh
@@ -0,0 +1,23 @@
+ssh bandit0@bandit.labs.overthewire.org -p 2220
+ls
+cat readme
+
+ZjLjTmM6FvvyRnrb2rfNWOZOTa6ip5If
+
+
+
+ssh bandit1@bandit.labs.overthewire.org -p 2220
+ls
+for read "-" named file 
+cat ./-
+
+263JGJPfgU6LtdEvgfWU1XP5yac29mFx
+
+
+
+ssh bandit2@bandit.labs.overthewire.org -p 2220
+ls
+for read "--spaces in this filename--" named file
+cat "./--spaces in this filename--"
+
+MNk8KNH3Usiio41PRUEoDFPqfxLPlSmx
